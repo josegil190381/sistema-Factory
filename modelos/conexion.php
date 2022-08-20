39827df46fc32669e72b08f338ebd6d0a3ec0840
josegil190381 <josegil190381@gmail.com>
2022-08-19 23:21:05 -0400

    Update conexion.php

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=aquanat",
+		$link = new PDO("mysql:host=localhost;dbname=sistema",
 			            "root",
 			            "");
 
